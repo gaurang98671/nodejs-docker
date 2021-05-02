b87de1cab7fe1602ad095cbeaa8b5b6a9393dd67
gaurang98671 <56935661+gaurang98671@users.noreply.github.com>
2021-05-02 19:24:14 +0530

    added mongo db package

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ RUN if [ "$NODE_ENV" = "development" ]; \
         then npm install; \
         else npm install --only=production; \
         fi
-        
+
 COPY . ./
 ENV PORT=3000
 EXPOSE $PORT
--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const mongo = require('mongoose');
+mongo.connect("mongodb://gaurang:123@mongo:27017/?authSource=admin"
+).then(()=>{
+    console.log("Successfully connected to database...")
+}).catch((e)=>{
+    console.log("error"+ e)
+});
 
 const app = express();
 

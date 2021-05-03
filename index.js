5bfee25d291821c696c4b5a15bb622ba4641a040
gaurang98671 <56935661+gaurang98671@users.noreply.github.com>
2021-05-04 03:06:33 +0530

    added redis container
    redis connection working
    created express-sessions with redis

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const mongo = require('mongoose');
-const { MONGO_IP, MONGO_USER, MONGO_PASSWORD, MONGO_PORT } = require('./config/configurations');
+const { MONGO_IP, MONGO_USER, MONGO_PASSWORD, MONGO_PORT, REDIS_URL, REDIS_PORT, REDIS_SECRET } = require('./config/configurations');
 const AuthRouter = require('./router/authRouter');
 const router = require('./router/postRoutes');
+var session = require('express-session')
+const redis = require('redis')
+let RedisStore = require('connect-redis')(session)
+
+//Connect to redis container
+let redisClient = redis.createClient(
+    {
+        host:  REDIS_URL,
+        port: REDIS_PORT
+    }
+)
+
 const app = express();
 
-
+//Connect to mongo container
 const retryMongoConnect = ()=>{
 const mongo_url = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
 mongo.connect(mongo_url
@@ -21,6 +33,12 @@ mongo.connect(mongo_url
 retryMongoConnect()
 
 app.use(express.urlencoded({extended : true}));
+app.use(session({
+    store: new RedisStore({ client : redisClient }), 
+    secret: REDIS_SECRET,
+    resave: true,
+    saveUninitialized: true
+}));
 app.use(express.json()); 
 app.use("/api",router)
 app.use("/auth", AuthRouter)
